901f46acf5a93cf274703d87201a54e2f206853d
Dmitry Palamarchuk <moonbrv@gmail.com>
2019-04-16 00:12:14 +0300

    add initial state shape

--- src/edit_table_task/core.cljs
+++ src/edit_table_task/core.cljs
@@ -6,18 +6,29 @@
 ;; ----- UTILS -----
 
 ;; ----- MODEL -----
-(defonce app-state (atom {:text "App state"}))
+(def initial-state-model {:headers {:company-header ""
+                                    :income-header ""}
+                          :table-values {"369deaef-c6ee-4836-9f74-e9593233abc8" {:company "Ace"
+                                                                                 :income 42}
+                                         "072da7c1-f5a6-4dee-bd2f-ab29b7273410" {:company "Acme"
+                                                                                 :income 7}
+                                         "419fe665-625a-4857-a1b8-cf1ebf287944" {:company "Evil"
+                                                                                 :income 28}}
+                          :uploaded true
+                          :error false})
+
+(defonce app-state (atom initial-state-model))
 
 ;; ----- VIEWS -----
-(rum/defc app-root []
-  [:div
-   [:h1.title "App content"]])
+(rum/defc app-root < rum/reactive []
+  [:section.section
+   [:h1.title "App content"]
+   [:div.container [:pre (str (rum/react app-state))]]])
 
 (defn mount-app-element []
   (when-let [el (gdom/getElement "app")]
     (rum/mount (app-root) el)))
 
-;; specify reload hook with ^;after-load metadata
 (defn ^:after-load on-reload []
   (mount-app-element)
 )
