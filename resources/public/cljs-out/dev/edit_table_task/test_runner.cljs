2cd7ae344a6443f6a50e3d910b7fd3629fbe01fc
Dmitry Palamarchuk <moonbrv@gmail.com>
2019-04-15 23:06:14 +0300

    remove old resource files

--- resources/public/cljs-out/dev/edit_table_task/test_runner.cljs
+++
@@ -1,9 +0,0 @@
-;; This test runner is intended to be run from the command line
-(ns edit-table-task.test-runner
-  (:require
-    ;; require all the namespaces that you want to test
-    [edit-table-task.core-test]
-    [figwheel.main.testing :refer [run-tests-async]]))
-
-(defn -main [& args]
-  (run-tests-async 5000))
