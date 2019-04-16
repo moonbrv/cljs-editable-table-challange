a9753d4e7107711b170cf3e21078050f19abe4aa
Dmitry Palamarchuk <moonbrv@gmail.com>
2019-04-16 22:10:12 +0300

    feat: make table editable
    changelog:
    - create input component
    - make table body fields editable
    - make table header fields editable
    - add table footer with total sum of income

--- src/edit_table_task/core.cljs
+++ src/edit_table_task/core.cljs
@@ -1,13 +1,13 @@
 (ns ^:figwheel-hooks edit-table-task.core
   (:require
-   [goog.dom :as gdom]
-   [rum.core :as rum]))
+    [goog.dom :as gdom]
+    [rum.core :as rum]))
 
 ;; ----- UTILS -----
 
 ;; ----- MODEL -----
 (def initial-state-model {:table-headers {:company-header "Company"
-                                    :income-header "Income"}
+                                          :income-header "Income"}
                           :table-values {"369deaef-c6ee-4836-9f74-e9593233abc8" {:company "Ace"
                                                                                  :income 42}
                                          "072da7c1-f5a6-4dee-bd2f-ab29b7273410" {:company "Acme"
@@ -20,19 +20,48 @@
 (defonce state (atom initial-state-model))
 
 ;; ----- VIEWS -----
+(rum/defc table-input [value type change-handler]
+  [:input {:value value
+           :type type
+           :on-change change-handler}])
+
+
 (rum/defc table-row [row-data]
   (let [data-key (get row-data 0)
         data (get row-data 1)]
-    [:tr [:td (:company data)] [:td (:income data)]]))
+    [:tr
+     [:td (table-input
+            (:company data)
+            "text"
+            (fn [e]
+              (swap! state assoc-in [:table-values data-key :company]
+                     (.. e -target -value))))]
+     [:td (table-input
+            (:income data)
+            "number"
+            (fn [e]
+              (swap! state assoc-in [:table-values data-key :income]
+                     (js/Number (.. e -target -value)))))]]))
 
 (rum/defc editable-table < rum/reactive  []
-  (let [headers (vals (:table-headers (rum/react state)))
-        table-data (vec (:table-values (rum/react state)))]
+  (let [headers (vec (:table-headers (rum/react state)))
+        table-data (vec (:table-values (rum/react state)))
+        total-sum (reduce (fn [acc n]
+                            (+ acc (get-in n [1 :income] 0)))
+                          0 table-data)]
     [:table.table
      [:thead [:tr
-              (for [title headers] [:th {:key title} title])]]
+              (for [column-header headers]
+                (let [header-key (get column-header 0)
+                      title (get column-header 1)]
+                  [:th {:key (name header-key)}
+                   (table-input title "text" (fn [e]
+                                               (swap! state assoc-in
+                                                      [:table-headers header-key]
+                                                      (.. e -target -value))))]))]]
      [:tbody (for [row-data table-data]
-               (rum/with-key (table-row row-data) (get row-data 0)))]]))
+               (rum/with-key (table-row row-data) (get row-data 0)))]
+     [:tfoot [:tr [:td "Total"] [:td total-sum]]]]))
 
 (rum/defc app-root < rum/reactive []
   [:section.section
@@ -45,5 +74,4 @@
     (rum/mount (app-root) el)))
 
 (defn ^:after-load on-reload []
-  (mount-app-element)
-)
+  (mount-app-element))
